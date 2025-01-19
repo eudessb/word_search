a50e94386d9b0342564a2a15de512657bc709adf
Eudes Silva <eudessilvabezerra985@gmail.com>
2025-01-18 21:06:54 -0300

    updated version

--- include/avl.h
+++ include/avl.h
@@ -1,4 +1,42 @@
 #ifndef AVL_H
 #define AVL_H
 
+#define MAX_TAMANHO_PALAVRA 99
+
+/**
+ * Estrutura criada ao perceber que teria que armazenar de alguma maneira as coordenadas das palavras encontradas.
+ */
+typedef struct PalavraEncontrada
+{
+  char palavra[MAX_TAMANHO_PALAVRA];
+  int x_inicio, y_inicio; //coordenadas x e y da primeira letra
+  int x_fim, y_fim; // coordenadas x e y da última letra
+} Palavra;
+
+typedef struct No
+{
+  Palavra *dado;
+  struct No *esquerdo;
+  struct No *direito;
+  int altura;
+} No;
+
+int obter_altura(No *no);
+
+int calcular_fator_de_balanceamento(No *no);
+
+No *criar_no(Palavra *dado);
+
+No *rotacao_direita(No *y);
+
+No *rotacao_esquerda(No *x);
+
+No *inserir_no(No *no, Palavra *dado);
+
+No *menor_valorNo(No *no);
+
+No *remover_no(No *raiz, const char *dado);
+
+void imprimir_em_ordem(No *raiz);
+
 #endif
--- include/trie.h
+++ include/trie.h
@@ -1,5 +1,27 @@
 #ifndef TRIE_H
 #define TRIE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#define ALFABETO 26
+
+/*
+'NO' virou 'NODE' para evitar confusões com as funções da AVL que usam uma struct NO.
+*/
+typedef struct Node
+{
+    struct Node *filhos[ALFABETO];
+    bool fim_da_palavra;
+} Node;
+
+bool buscar(Node *raiz, const char *palavra);
+
+Node *criar_Node();
+
+void inserir(Node *raiz, const char *palavra);
+
+void liberar_trie(Node *raiz);
 
 #endif
